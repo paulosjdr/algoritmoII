7d78e399dad55e60e9f202e666766053a3dffda2
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-08-02 22:06:30 -0300

    Create aula02082023_revisao_ex4.c

---
+++ aula02082023_revisao_ex4.c
@@ -0,0 +1,35 @@
+//INFORME A NOTA: 55 (EXEMPLO)
+//1.VOCÊ FOI APROVADO
+//2.VOCÊ FOI REPROVADO
+//3.VOCÊ ESTÁ DE RECUPERAÇÃO
+//OTIMIZAR O RECURSO IF
+//COLOCAR TRÊS ALUNOS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    float nota;
+
+    setlocale(LC_ALL, "");
+
+    for(int i=0; i<=2; i++){
+        printf("\nINFORME A NOTA DO ALUNO %d: \n", i+1);
+        scanf("%f", &nota);
+
+        if(nota>=60){
+            printf("VOCÊ FOI APROVADO!\n");
+        } else if(nota<30){
+            printf("\nVOCÊ FOI REPROVADO!\n");
+        } else {
+            printf("\nRECUPERAÇÃO!\n");
+        }
+
+        printf("SUA NOTA É: %.2f", nota);
+
+    }
+
+    return 0;
+}
